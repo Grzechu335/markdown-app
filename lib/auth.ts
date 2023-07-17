622ff443943dba0191a41fb5037dec4efaa3415a
Grzegorz <grzegorz.skrabucha@gmail.com>
2023-07-17 17:08:55 +0200

    created and integrated with database prisma client

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,13 @@
 import type { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { prisma } from './prisma'
 
 export const authOptions: NextAuthOptions = {
     pages: {
         signIn: '/auth/signin',
     },
+    adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
