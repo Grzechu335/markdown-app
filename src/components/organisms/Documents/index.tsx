6be1cb7fabcb60a7c18f65ebf949e228d0271346
Grzegorz <grzegorz.skrabucha@gmail.com>
2023-07-18 11:42:59 +0200

    Added skeleton while fetching all markdown files

--- src/components/organisms/Documents/index.tsx
+++ src/components/organisms/Documents/index.tsx
@@ -5,40 +5,53 @@ import { useMarkdownContext } from '../../../../context/MarkdownContext'
 import useGetAllFiles from '../../../../hooks/useGetAllFiles'
 import { clsxm } from '../../../../utils/clsxm'
 import dateFormatter from '../../../../utils/dateFormatter'
+import Skeleton from 'react-loading-skeleton'
 
 const Documents: React.FC = () => {
     const { changeSelectedFileId, selectedFileId } = useMarkdownContext()
-    const { files: markdowns } = useGetAllFiles()
+    const { files: markdowns, loading } = useGetAllFiles()
     return (
-        <div className="space-y-[26px] h-full flex-grow overflow-y-scroll scrollbar-none overflow-x-hidden">
-            {markdowns?.map((file) => (
-                <div
-                    key={file.id}
-                    className={clsxm(
-                        'flex items-center px-3 py-1 space-x-4 rounded-md cursor-pointer group',
-                        {
-                            'dark:bg-300/20': file.id === selectedFileId,
-                        }
-                    )}
-                    onClick={() => changeSelectedFileId(file.id)}
-                >
-                    <FileIcon
-                        size={18}
-                        color="#fff"
-                        className="flex-shrink-0"
-                    />
+        <>
+            {!loading ? (
+                <div className="space-y-[26px] h-full flex-grow overflow-y-scroll scrollbar-none overflow-x-hidden">
+                    {markdowns?.map((file) => (
+                        <div
+                            key={file.id}
+                            className={clsxm(
+                                'flex items-center px-3 py-1 space-x-4 rounded-md cursor-pointer group',
+                                {
+                                    'dark:bg-300/20':
+                                        file.id === selectedFileId,
+                                }
+                            )}
+                            onClick={() => changeSelectedFileId(file.id)}
+                        >
+                            <FileIcon
+                                size={18}
+                                color="#fff"
+                                className="flex-shrink-0"
+                            />
 
-                    <div className="w-full">
-                        <span className="body-md text-500">
-                            {dateFormatter(new Date(file.lastUpdate))}
-                        </span>
-                        <p className="flex-grow-0 block truncate heading-md text-100 group-hover:text-orange">
-                            {file.name}
-                        </p>
-                    </div>
+                            <div className="w-full truncate">
+                                <span className="body-md text-500">
+                                    {dateFormatter(new Date(file.lastUpdate))}
+                                </span>
+                                <p className="flex-grow-0 block truncate heading-md text-100 group-hover:text-orange">
+                                    {file.name}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            ))}
-        </div>
+            ) : (
+                Array.from({ length: 10 }).map((_, idx) => (
+                    <Skeleton
+                        key={idx}
+                        className="w-full h-10"
+                    />
+                ))
+            )}
+        </>
     )
 }
 
