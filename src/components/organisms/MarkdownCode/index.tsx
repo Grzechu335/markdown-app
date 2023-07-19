db12dc33f78c069abd066b2ff8fa5c8accaaba54
Grzegorz <grzegorz.skrabucha@gmail.com>
2023-07-19 12:55:20 +0200

    UI fixes

--- src/components/organisms/MarkdownCode/index.tsx
+++ src/components/organisms/MarkdownCode/index.tsx
@@ -6,7 +6,7 @@ import { useMarkdownContext } from '../../../../context/MarkdownContext'
 const MarkdownCode: React.FC = () => {
     const { input, changeInput } = useMarkdownContext()
     return (
-        <div className="absolute flex flex-col flex-1 w-full h-full md:static ">
+        <div className="absolute flex flex-col flex-1 w-full h-full pb-10 md:static">
             <MarkdownHeading>Markdown</MarkdownHeading>
             <textarea
                 value={input}
