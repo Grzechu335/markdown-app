9cee6d7fdd129f542efd3871691cc974006e0e45
Grzegorz <grzegorz.skrabucha@gmail.com>
2023-07-20 22:49:57 +0200

    Added server side user auth and loading spinner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import HomePageTemplate from '@/components/templates/HomePageTemplate'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../../lib/auth'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
+export default async function Home() {
+    const session = await getServerSession(authOptions)
+    if (!session) redirect('/auth/signin')
     return <HomePageTemplate />
 }
--- src/components/templates/HomePageTemplate.tsx
+++ src/components/templates/HomePageTemplate.tsx
@@ -12,16 +12,8 @@ import { clsxm } from '../../../utils/clsxm'
 import MarkdownCode from '../organisms/MarkdownCode'
 import MarkdownPreview from '../organisms/MarkdownPreview'
 import useGetAllFiles from '../../../hooks/useGetAllFiles'
-import ReactFocusLock from 'react-focus-lock'
 
 const HomePageTemplate: React.FC = () => {
-    const router = useRouter()
-    const session = useSession({
-        required: true,
-        onUnauthenticated: () => {
-            router.push('/auth/signin')
-        },
-    })
     const { sidebar, preview, togglePreview } = useUIContext()
     const { files } = useGetAllFiles()
     return (
